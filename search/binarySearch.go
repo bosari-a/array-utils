7d3a9c54a22e74196029aabf98401040dff9f885
bosari-a <ahmedelbosarimc@gmail.com>
2024-01-10 09:39:34 +0400

    added more comments to code, added param for sorting while initializing array

--- search/binarySearch.go
+++ search/binarySearch.go
@@ -1,35 +1,93 @@
 package search
 
-type BinaryInt struct {
-	IntArray []int
-	Low      int
-	High     int
-	Mid      int
+import "sort"
+
+// Struct for a int array with binary search method and extra properties
+type binarySearchInt struct {
+	intArray []int
+	low      int
+	high     int
+	mid      int
 }
 
-func BinaryIntFactory(a []int) *BinaryInt {
-	var bi BinaryInt
-	bi.IntArray = a
+// Initialize a struct with binary search method for int array
+// The struct contains an array that will be sorted based on the sorted boolean param
+func BinaryIntFactory(a []int, sorted bool) *binarySearchInt {
+	var bi binarySearchInt
+	if !sorted {
+		sort.Slice(a, func(i, j int) bool {
+			return a[i] < a[j]
+		})
+	}
+
+	bi.intArray = a
 	l := len(a)
-	bi.High = l - 1
-	bi.Low = 0
-	bi.Mid = (l - 1) / 2
+	bi.high = l - 1
+	bi.low = 0
+	bi.mid = (l - 1) / 2
 	return &bi
 }
 
-func (a *BinaryInt) BinarySearchInt(find int) int {
-	p := a.IntArray[a.Mid]
+// Method to search int array using binary search
+// Returns the index of the element in the array if it is found
+// Returns -1 if the element is not found
+func (a *binarySearchInt) BinarySearchInt(find int) int {
+	p := a.intArray[a.mid]
 	if find == p {
-		return a.Mid
-	} else if a.Low == a.Mid && a.High == a.Mid {
+		return a.mid
+	} else if a.low == a.mid && a.high == a.mid {
 		return -1
 	}
 	if find > p {
-		a.Low = a.Mid + 1
-		a.Mid = (a.Low + a.High) / 2
+		a.low = a.mid + 1
+		a.mid = (a.low + a.high) / 2
 		return a.BinarySearchInt(find)
 	}
-	a.High = a.Mid - 1
-	a.Mid = (a.Low + a.High) / 2
+	a.high = a.mid - 1
+	a.mid = (a.low + a.high) / 2
 	return a.BinarySearchInt(find)
+}
+
+// Struct for a string array with binary search method and extra properties
+type binarySearchStr struct {
+	strArray []string
+	low      int
+	high     int
+	mid      int
+}
+
+// Initialize a struct with binary search method for string array
+// The struct contains an array that will be sorted based on the sorted boolean param
+func BinaryStrFactory(s []string, sorted bool) *binarySearchStr {
+	var bs binarySearchStr
+	if !sorted {
+		sort.Slice(s, func(i, j int) bool {
+			return s[i] < s[j]
+		})
+	}
+
+	bs.strArray = s
+	l := len(s)
+	bs.high = l - 1
+	bs.low = 0
+	bs.mid = (l - 1) / 2
+	return &bs
+}
+
+// Method that implements binary search on an array of strings
+func (bs *binarySearchStr) BinarySearchStr(find string) int {
+	s := bs.strArray[bs.mid]
+	if find == s {
+		return bs.mid
+	} else if bs.low == bs.mid && bs.high == bs.mid {
+		return -1
+	}
+	if find > s {
+		bs.low = bs.mid + 1
+		bs.mid = (bs.low + bs.high) / 2
+		return bs.BinarySearchStr(find)
+	}
+	bs.high = bs.mid - 1
+	bs.mid = (bs.low + bs.high) / 2
+	return bs.BinarySearchStr(find)
 }
